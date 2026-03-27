a208b390829a487ab250e732ff9f5e97006e6115
RanaNoumanAshfaq <noumanrananouman59@gmail.com>
2026-03-27 16:32:19 +0500

    Enhance security middleware

--- frontend/app.js
+++ frontend/app.js
@@ -4,3 +4,6 @@ console.log('Build shipment core module');
 
 // Improve frontend styling system - 2026-04-11 19:40:41
 console.log('Improve frontend styling system');
+
+// Enhance security middleware - 2026-03-27 16:32:19
+console.log('Enhance security middleware');
