41c0641285162d6c1bea9e8204c6bfeaddce7f8f
RanaNoumanAshfaq <noumanrananouman59@gmail.com>
2026-04-10 02:15:28 +0500

    Optimize frontend performance

--- backend/api.js
+++ backend/api.js
@@ -7,3 +7,6 @@ console.log('Add cargo booking feature');
 
 // Improve logistics calculation engine - 2026-04-18 11:43:20
 console.log('Improve logistics calculation engine');
+
+// Optimize frontend performance - 2026-04-10 02:15:28
+console.log('Optimize frontend performance');
