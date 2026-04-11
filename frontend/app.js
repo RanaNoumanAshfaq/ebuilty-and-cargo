feaed34c4f31f3114e6f6e47a6d6ba905009e6ec
RanaNoumanAshfaq <noumanrananouman59@gmail.com>
2026-04-11 19:40:41 +0500

    Improve frontend styling system

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,6 @@
 
 // Build shipment core module - 2026-05-21 01:53:02
 console.log('Build shipment core module');
+
+// Improve frontend styling system - 2026-04-11 19:40:41
+console.log('Improve frontend styling system');
