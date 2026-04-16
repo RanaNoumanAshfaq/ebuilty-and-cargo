bd6d870c46e5415fceafd03fb44b89cec214c305
SauodAzad <sauodazd@gmail.com>
2026-04-17 04:04:06 +0500

    feat: improve billing precision logic

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,3 +13,6 @@ console.log('fix: correct dashboard rendering issue');
 
 // fix: handle tracking edge case error - 2026-05-07 23:34:25
 console.log('fix: handle tracking edge case error');
+
+// feat: improve billing precision logic - 2026-04-17 04:04:06
+console.log('feat: improve billing precision logic');
