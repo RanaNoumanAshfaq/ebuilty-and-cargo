6f808a9001bc7d43f2fffa1e23ea4f711466a649
RanaNoumanAshfaq <noumanrananouman59@gmail.com>
2026-04-18 11:43:20 +0500

    Improve logistics calculation engine

--- backend/api.js
+++ backend/api.js
@@ -4,3 +4,6 @@ console.log('Initialize eCargo backend structure');
 
 // Add cargo booking feature - 2026-05-29 10:44:00
 console.log('Add cargo booking feature');
+
+// Improve logistics calculation engine - 2026-04-18 11:43:20
+console.log('Improve logistics calculation engine');
