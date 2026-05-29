584c93051d5ff82105caa6e9e7dcdf2eeb072105
RanaNoumanAshfaq <noumanrananouman59@gmail.com>
2026-05-29 10:44:00 +0500

    Add cargo booking feature

--- backend/api.js
+++ backend/api.js
@@ -1,3 +1,6 @@
 
 // Initialize eCargo backend structure - 2026-05-17 08:53:48
 console.log('Initialize eCargo backend structure');
+
+// Add cargo booking feature - 2026-05-29 10:44:00
+console.log('Add cargo booking feature');
